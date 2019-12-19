e5e30663cdece18f2588d7ba53ed6e6da61eb078
lgoyal06 <vaRaNa8!@#>
2019-12-20 05:25:26 +0530

    Implemented Heap sort and heapify behaviour to Heap DS

--- src/com/lalit/ds/basic/heap/HeapDSUtils.java
+++ src/com/lalit/ds/basic/heap/HeapDSUtils.java
@@ -1,39 +1,51 @@
 package com.lalit.ds.basic.heap;
 
+import java.util.function.Consumer;
+
 //Usage Heap Data Structure
+//TODO Do a visit again, try to understand again 21 December 2019
 //https://www.youtube.com/watch?v=B7hVxCmfPtM&t=2570s
-//1. Find kth max /min element  - learn about total time complexity to find kth max element
-//2.Heap sort
-//3.Priority queue to find max/min element at top
+//Operations
+//Delete
+//Insert
+//Heapify random array
+//Heap Sort
 public class HeapDSUtils {
 
     private static int[] heapifiedArray;
 
-    //TODO Max Heapify fix it, it is not correct 11th December 2019
-    //https://www.youtube.com/watch?v=HqPJF2L5h9U Time 42:50
+    //Time Complexity is O(n) to heapify random array
     public static int[] maxHeapify(int[] arrayToSort) {
         int[] sortedArray = arrayToSort;
-        for (int i = ((arrayToSort.length) - 1) / 2; i >= 0; --i) {
+        for (int i = ((arrayToSort.length) / 2 - 1); i >= 0; --i) {
             sortedArray = maxHeapify(arrayToSort, i);
         }
         return sortedArray;
     }
 
-    //Time Complexity is O(n) to get highest element on top of array
     private static int[] maxHeapify(int[] array, int i) {
-        int leftChild = 2 * i;
-        int rightChild = (2 * i) + 1;
-        int largestValIndex = i;
-        if (leftChild < array.length && array[leftChild] > array[largestValIndex]) {
-            largestValIndex = leftChild;
-        }
-        if (rightChild < array.length && array[rightChild] > array[largestValIndex]) {
-            largestValIndex = rightChild;
-        }
-        if (largestValIndex != i) {
-            array = swap(array, largestValIndex, i);
+        while (isNonLeafNode(i, array.length)) {
+            int largestValIndex = i;
+            int leftChild = (2 * i) + 1;
+            int rightChild = (2 * i) + 2;
+            if (leftChild < array.length && array[leftChild] > array[largestValIndex]) {
+                largestValIndex = leftChild;
+            }
+            if (rightChild < array.length && array[rightChild] > array[largestValIndex]) {
+                largestValIndex = rightChild;
+            }
+            if (largestValIndex != i) {
+                array = swap(array, largestValIndex, i);
+                i = largestValIndex;
+            } else {
+                return array;
+            }
         }
         return array;
+    }
+
+    private static boolean isNonLeafNode(int i, int i2) {
+        return i <= (i2 / 2 - 1);
     }
 
     private static int[] swap(int[] array, int indexA, int indexB) {
@@ -110,31 +122,44 @@ public class HeapDSUtils {
         }
     }
 
-    //TODO Heapsort 1st December 2019
-    //https://www.youtube.com/watch?v=HqPJF2L5h9U Time 42:50
+    // Heapify O(n)
+    // Deletion O(nlogn)
+    //Total - O(n)+O(nlogn)
     public static int[] heapSort(int[] array) {
-        return null;
+        int[] sortedArray = new int[array.length];
+        int arrayLength = array.length;
+        for (int i = 0; i < arrayLength; ++i) {
+            sortedArray[i] = delete();
+        }
+        return sortedArray;
     }
 
     public static void main(String... s) {
-        //Fetch Max value one by one
-        //Priority Queue with Max value at top
-        int[] array = new int[]{3, 43, 2, 5343, 111123, 343};
-        heapifiedArray = new int[0];
-        insert(3);
-        insert(43);
-        insert(2);
-        insert(5343);
-        insert(111123);
-        insert(343);
-        insert(12);
-        insert(1221212211);
-        insert(34);
-        insert(36);
-        insert(78);
-        insert(1);
-        for (int i = 0; i < 12; ++i)
-            System.out.println(delete());
+        int[] array = new int[]{10, 20, 15, 12, 40, 25, 33, 2322, 18, 34, 6, 56, 3, 2, 56, 222, 3, 1, 343434};
+        /**heapifiedArray = new int[0];
+         insert(3);
+         insert(43);
+         insert(2);
+         insert(5343);
+         insert(111123);
+         insert(343);
+         insert(12);
+         insert(1221212211);
+         insert(34);
+         insert(36);
+         insert(78);
+         insert(1);
+         for (int i = 0; i < 12; ++i)
+         System.out.println(delete());**/
+        heapifiedArray = maxHeapify(array);
+        int heapSize = heapifiedArray.length;
+        //for (int i = 0; i < heapSize; ++i)
+        //    System.out.println(delete());
+        int[] sortedArray = heapSort(heapifiedArray);
+        Consumer<Integer> consumer = (val) -> System.out.println(val);
+        for (int ele : sortedArray) {
+            consumer.accept(ele);
+        }
     }
 
 
